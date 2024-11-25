6a2b621e4f45f5eb2a5152985cbbd6c74b0ce870
JoseTejero <139390301+JoseDeJesusTejeroZapata@users.noreply.github.com>
2024-11-25 10:50:08 -0600

    Primeras 3 lecciones JavaScript

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,48 @@
+// Tipos de datos primitivos
+
+// Cadenas de texto (string)
+let myName = "José Tejero"
+let alias = 'Pepe'
+let email = `j_jtejero@hotmail.com`
+
+// Números (number)
+let age = 35 // Entero
+let height = 1.72 // Decimal
+
+// Booleanos (boolean)
+let isTeacher = true
+let isStudent = false
+
+// Undefined
+let undefinedValue
+// console.log(undefinedValue)
+
+// Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let myBigInt = BigInt(1230498173204987123409817230498172340987)
+let myBigInt2 = 1230498173204987123409817230498172340987n
+
+// Mostramos los tipos de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
