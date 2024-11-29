38f44e40d922ddab52d26fc4aa6c6a2a7f98ee79
Jose-Tejero <j_jtejero@hotmail.com>
2024-11-29 00:25:52 -0600

    mi contribución u.u

---
+++ Basic/12-map.js
@@ -0,0 +1,19 @@
+// Map
+
+// Declaración
+
+let myMap = new Map()
+
+console.log(myMap)
+
+// Inicialización
+
+myMap = new Map([
+  ["name", "Jose"],
+  ["email", "jose@correo.com"],
+  ["age", 35],
+])
+
+console.log(myMap)
+
+// Métodos y propiedades
