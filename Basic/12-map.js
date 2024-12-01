3178e99b13dc7cb3c9ce067794563496886b8cfe
Jose-Tejero <j_jtejero@hotmail.com>
2024-11-30 22:05:16 -0600

    Aprendiendo map

--- Basic/12-map.js
+++ Basic/12-map.js
@@ -17,3 +17,42 @@ myMap = new Map([
 console.log(myMap)
 
 // Métodos y propiedades
+
+// set
+
+myMap.set("alias", "pepegrisho")
+myMap.set("name", "Jesús")
+
+console.log(myMap)
+
+// get
+
+console.log(myMap.get("email"))
+console.log(myMap.get("apeshido"))
+
+// has
+
+console.log(myMap.has("apeshido"))
+console.log(myMap.has("age"))
+
+// delete
+
+myMap.delete("email")
+
+console.log(myMap)
+
+// keys, values y entries
+
+console.log(myMap.keys())
+console.log(myMap.values())
+console.log(myMap.entries())
+
+// size
+
+console.log(myMap.size)
+
+// clear
+
+myMap.clear()
+
+console.log(myMap)
